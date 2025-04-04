b72a9711b2882181d8238046098bac9cdfc7bdf5
diya-jpg <diyabari2004@gmail.com>
2025-04-04 21:55:41 +0530

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -3,24 +3,27 @@ import UserClass from "./UserClass";
 class About extends Component{
     constructor(props){
         super(props);
-        console.log("Parent Constructor")
+       // console.log("Parent Constructor")
     }
-    componentDidMount(){
-console.log("component did mount");
+   componentDidMount(){
+//console.log("component did mount");
 
     }
     render(){
-        console.log("Parent Render")
+        //console.log("Parent Render")
         return(
            
             <div >
           
             <h1>About Us</h1>
             <h2>This is Namaste React Web Series</h2>
-            
-            <UserClass name={"Diya Bari(class)"} location={"Jaipur"}/>
+              <UserClass name={"Diya Bari(class)"} location={"Jaipur"}/>
         </div>
         );
     }
 }
+
+
+
+
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,26 +3,42 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            count:0,
+         userInfo:{
+            name:"Dummy",
+            location:"Default",
+            avatar_url:"https://hips.hearstapps.com/hmg-prod/images/dahlia-1508785047.jpg?crop=1.00xw:0.669xh;0,0.0136xh&resize=980:*"
+         },
         
         };
-        console.log("child constructor");
-    };
+       // console.log("child constructor");
+    }
+   async componentDidMount(){
+       // console.log("Child Component Did Mount");
+
+       const data=await fetch("https://api.github.com/users/diya-jpg");
+const json= await data.json();
+this.setState({
+    userInfo:json,
+})
+    }
     render(){
-        const{name,location}=this.props;
-        const{count}=this.state;
-        console.log("child render");
+        // const{name,location}=this.props;
+        // const{count}=this.state;
+        //console.log("child render");
+        const {name,location,avatar_url}=this.state.userInfo;
         return(
             <div className="user-card">
-                <h1>Count:{count}</h1>
+               {/* <h1>Count:{count}</h1>
                <button onClick={()=>{this.setState({
                 count:this.state.count+1,
                });
                }}>
                 Count Increases
-               </button>
+               </button> */}
+               <img src={avatar_url}/>
             <h2>Name:{name}</h2>
             <h3>Location:{location}</h3>
+            
           <h4>Contact:@diyabari</h4>
         </div>
         )
